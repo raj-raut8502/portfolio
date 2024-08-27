452c007bf7018fd591283049275394f122b9234b
pratikshaa-23 <158586762+pratikshaa-23@users.noreply.github.com>
2024-08-27 12:21:09 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -44,7 +44,7 @@ const Navbar = () => {
         >
           <img src={logo} alt='logo' className='w-9 h-9 object-contain' />
           <p className='text-white text-[18px] font-bold cursor-pointer flex '>
-            Raj &nbsp;
+            Pratiksha &nbsp;
             <span className='sm:block hidden'> | DevOps Engineer</span>
           </p>
         </Link>
